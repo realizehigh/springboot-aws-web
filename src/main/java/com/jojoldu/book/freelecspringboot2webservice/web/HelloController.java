db835128e4ba8175622e8e35c06ed8fd91f1e472
Heonjun Jeong <gjswns1991@gmail.com>
2021-12-27 16:23:13 +0900

    HelloController 코드를 롬복으로 전환

--- src/main/java/com/jojoldu/book/freelecspringboot2webservice/web/HelloController.java
+++ src/main/java/com/jojoldu/book/freelecspringboot2webservice/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.freelecspringboot2webservice.web;
 
+import com.jojoldu.book.freelecspringboot2webservice.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping
+    public HelloResponseDto helloDto(@RequestParam("name") String name, // 1
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name,amount);
     }
 }
